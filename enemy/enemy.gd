e2e2a330fc128049ad23f2dbf15201d246459af4
Wetia <jojo2schmitz@web.de>
2020-08-06 23:57:47 +0200

    Added a mini demo map Added Astar for enemis Added a find_way func for enemis

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -11,28 +11,50 @@ export (int) var gold_value = 5
 export (int) var max_health
 var health
 
-var last_tower_hit = null
-var tags
+var way_points = []
+var move_direction = Vector2(0, 0)
+
 var dead = false
+var offset_pos = Vector2(32, 32)
 
 var velocity = Vector2()
 
+var astar
+
 func _ready():
-	global_position =  Vector2(0, 120)
-		
+	spawn( get_parent().get_pos_on_map_mid(Vector2(0, 0)))
+	find_way(Vector2(7, 3))
+
 func spawn(_position):
 	global_position = _position
 	health = max_health
+		
+func _physics_process(delta):
+		if dead:
+			return
+		control(delta)		
+		move_and_slide(velocity)	
 
-func control(delta):
-	var direction = Vector2(-1, 0)
-	var changed_speed = speed			
-	velocity = direction * changed_speed * delta * -100
+
+func control(delta):	
+	var changed_speed = speed
+	calc_move_direction()	
+	velocity = move_direction * changed_speed * delta * -100
 	if $Animation.has_animation('walk'):
 		if 	!$Animation.is_playing():
 			$Animation.play('walk')		
 		if speed != changed_speed:
 			$Animation.playback_speed = changed_speed/speed
+
+func calc_move_direction():
+	if way_points.size() >= 1:
+		move_direction = (position - way_points[0]).normalized()
+		if position.distance_to(way_points[0]) < 1:
+			way_points.erase(way_points[0])		
+	else:
+		move_direction = Vector2(0, 0)	
+	print(position)
+	
 	
 func take_damage(damage):
 	if dead or damage == null:
@@ -43,29 +65,57 @@ func take_damage(damage):
 func die():
 	if get_parent().player:
 		get_parent().player.add_money(gold_value)
-	if last_tower_hit:
-		last_tower_hit.add_exp(experience)
 	dead()
 		
 func dead():
 	dead = true
 	queue_free()
-	
-func _physics_process(delta):
-		if dead:
-			return
-		control(delta)
-		move_and_slide(velocity)
 		
 func get_velocity():
 	return velocity
+	
+func find_way(_target):
+	ini_astar()
+	var path = astar.get_id_path(cor_to_id(Vector2(0,0)), cor_to_id(_target))	
+	for id in path:
+		way_points.append(get_parent().get_pos_on_map_mid(id_to_cor(id)))
+	
+	
+	
+	
+	
+	
+func ini_astar():
+	var map_size = get_parent().get_map_size()
+	var map_as_bi = get_parent().get_map_as_bi()
+	astar = AStar.new()
+	if astar == null:
+		astar = AStar.new()
+	else:
+		astar.clear()
+		
+	for x in range(map_size.x):
+		for y in range(map_size.y):
+			astar.add_point(cor_to_id(Vector2(x,y)), Vector3(x, y, 0))
+			
+	for x in range(map_size.x):
+		for y in range(map_size.y):
+			if map_as_bi[x][y]:
+				if x -1 >= 0:
+					if map_as_bi[x -1][y]:
+						astar.connect_points(cor_to_id(Vector2(x,y)), cor_to_id(Vector2(x -1,y)), true)
+				if y -1 >= 0:
+					if map_as_bi[x][y -1]:
+						astar.connect_points(cor_to_id(Vector2(x,y)), cor_to_id(Vector2(x ,y -1)), true)
+				
+			
+			
+	
 
-
-func load_settings(_settings):
-	if _settings[0][0]:
-		for s in _settings:
-			set(s[0],s[1])
-
-
-func is_Enemy():
-	return true
+	
+func cor_to_id(cor):
+	return cor.x * 10000 + cor.y
+	
+func id_to_cor(id):
+	return Vector2(stepify(id/10000, 1),id % 10000)
+	
---
+++ terrain/map.gd
@@ -0,0 +1,106 @@
+extends Node2D
+
+var width = 13
+var height = 8
+var map_as_bi = []
+
+
+func _init():
+
+	for x in range(width):
+		map_as_bi.append([])
+		for y in range(height):
+			map_as_bi[x].append(false)
+	map_as_bi[0][0] = true
+	map_as_bi[1][0] = true
+	map_as_bi[2][0] = true
+	map_as_bi[3][0] = true
+	map_as_bi[4][0] = true
+	map_as_bi[5][0] = true
+	map_as_bi[6][0] = true
+	map_as_bi[7][0] = true
+	map_as_bi[7][1] = true
+	map_as_bi[7][2] = true
+	map_as_bi[7][3] = true
+	map_as_bi[8][3] = true
+	map_as_bi[9][3] = true
+	map_as_bi[10][3] = true
+	map_as_bi[10][2] = true
+	map_as_bi[10][1] = true
+	map_as_bi[10][0] = true
+	map_as_bi[11][0] = true
+	map_as_bi[12][0] = true
+	
+	map_as_bi[0][7] = true
+	map_as_bi[1][7] = true
+	map_as_bi[2][7] = true
+	map_as_bi[3][7] = true
+	map_as_bi[4][7] = true
+	map_as_bi[5][7] = true
+	map_as_bi[6][7] = true
+	map_as_bi[7][7] = true
+	map_as_bi[8][7] = true
+	map_as_bi[9][7] = true
+	map_as_bi[10][7] = true
+	map_as_bi[11][7] = true
+	map_as_bi[12][7] = true
+	
+	map_as_bi[0][1] = true
+	map_as_bi[0][2] = true
+	map_as_bi[0][3] = true
+	map_as_bi[0][4] = true
+	map_as_bi[0][5] = true
+	map_as_bi[0][6] = true
+
+	map_as_bi[12][1] = true
+	map_as_bi[12][2] = true
+	map_as_bi[12][3] = true
+	map_as_bi[12][4] = true
+	map_as_bi[12][5] = true
+	map_as_bi[12][6] = true
+	
+	map_as_bi[2][2] = true
+	map_as_bi[3][2] = true
+	map_as_bi[4][2] = true
+	map_as_bi[4][3] = true
+	map_as_bi[4][4] = true
+	map_as_bi[4][5] = true
+	map_as_bi[4][6] = true
+
+			
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var dirt_block = load("res://terrain/blocks/dirt.tscn")
+	var air_block = load("res://terrain/blocks/air.tscn")
+	var block_inst
+	
+	for x in range(width):
+		map_as_bi.append([])
+		for y in range(height):
+			if map_as_bi[x][y]:
+				block_inst = air_block.instance()
+			else:
+				block_inst = dirt_block.instance()
+				
+			block_inst.global_position = Vector2(64 * x, 64 * y)
+			$blocks.add_child(block_inst)
+
+func get_map_size():
+	return Vector2(width, height)
+
+func get_map_as_bi():
+	return map_as_bi
+
+func get_root_offset():
+	return $blocks.global_position
+	
+func get_pos_on_map(_cor):
+	return _cor * Vector2(64, 64)
+
+func get_pos_on_map_mid(_cor):
+	return _cor * Vector2(64, 64) +  get_root_offset()
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
