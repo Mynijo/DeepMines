7aaaa963fdc761ae8b3ecbec89381024f9391797
Wetia <jojo2schmitz@web.de>
2021-03-16 00:39:36 +0100

    Added new relicts

---
+++ relic/GoldShovel.gd
@@ -0,0 +1,17 @@
+extends "res://relic/Relic.gd"
+
+export (int) var amount = 50
+
+# Declare member variables here. Examples:
+func _init():
+	$Tags.add_tag($Tags.e_relic.pick_up_me)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func call_on_pick_up_me():
+	Player.add_shovel(amount)
+
+func get_description():
+	return "Recive " + str(amount) + " Shovels"
