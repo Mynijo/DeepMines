acc44d2f4caa077c7caf2e65fa9177500ac91fe7
Wetia <jojo2schmitz@web.de>
2021-03-16 00:53:43 +0100

    Added new relcits

---
+++ relic/Rent.gd
@@ -0,0 +1,21 @@
+extends "res://relic/Relic.gd"
+
+var traps = []
+
+export (int) var amount = 10
+
+# Declare member variables here. Examples:
+func _init():
+	$Tags.add_tag($Tags.e_relic.game_stat_change)
+	
+	
+func _ready():
+	pass # Replace with function body.
+
+	
+func call_on_gamestate_changed(var _new_gamestate):
+	Player.add_money(amount)
+	
+	
+func get_description():
+	return "Add " + str(amount) + " money round"
