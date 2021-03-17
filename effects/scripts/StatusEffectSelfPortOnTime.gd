0aa6f020839c525b1df5c2724466fe5def9f627d
Wetia <jojo2schmitz@web.de>
2021-03-18 00:57:27 +0100

    Added SelfPortOnTime Jhins

--- effects/scripts/StatusEffectSelfPortOnTime.gd
+++ effects/scripts/StatusEffectSelfPortOnTime.gd
@@ -1,17 +1,20 @@
 extends "res://effects/scripts/StatusEffect.gd"
 
+export (float) var port_timer = 1
+
 func _init():
 	$Tags.add_tag($Tags.e_effect.buff)
 	$Tags.add_tag($Tags.e_effect.init)
-	$Tags.add_tag($Tags.e_effect.took_dmg)
 	$Tags.add_tag($Tags.e_effect.has_icon)
 	
 func effekt(value, tag):
 	if tag == $Tags.e_effect.init:
+		port_timer = rand_range(port_timer *  0.8, port_timer * 1.2)
+		$Timer.wait_time = port_timer
 		parent = value
-	if tag == $Tags.e_effect.took_dmg:
-		if value > 0:
-			parent.global_position = parent.way_points[0][0]
-		return value
+		$Timer.start()
 	
 
+func _on_Timer_timeout():
+	parent.global_position = parent.way_points[0][0]
+	$Timer.start()
